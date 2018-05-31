ee4498a431bcbf08dd5e25f4fe54b33abae99f1e
Renato Ramiro <renato.ramiro@virtus.ufcg.edu.br>
2018-05-31 13:31:52 -0300

    Improved layout and implemented some flows

--- renderer.js
+++ renderer.js
@@ -31,19 +31,83 @@ serialport.list((err, ports) => {
 
 });
 
+document.getElementById('selectPort').addEventListener('change', function () {
+  document.getElementById('code').disabled = true;
+  document.getElementById('saveData').disabled = true;
+});
+
+document.getElementById("checkPort").addEventListener('click', function() {
+  var select = document.getElementById('selectPort');
+  document.getElementById('error').style.visibility = 'hidden';
+  document.getElementById('success').style.visibility = 'hidden';
+  checkPort(select.value);
+});
+
+document.getElementById("saveData").addEventListener('click', function() {
+  document.getElementById('error').style.visibility = 'hidden';
+  document.getElementById('success').style.visibility = 'hidden';
+
+  var select = document.getElementById('selectPort');
+  var input = document.getElementById('code');
+  console.log(select.value, code.value);
+  
+  saveData(select.value, input.value);
+});
+
 function checkPort(port) {
-  var port = new SerialPort(port, { autoOpen: false });
+  var port = new serialport(port, { autoOpen: false });
  
   port.open(function (err) {
     if (err) {
-      return console.log('Error opening port: ', err.message);
+      document.getElementById('error').style.visibility = 'visible';
+      document.getElementById('error').textContent = err.message + " (" + new Date() + ")";
+      return;
     }
   
     // Because there's no callback to write, write errors will be emitted on the port:
-    port.write('main screen turn on');
+    port.write('ping');
 
     port.on('readable', function () {
-      console.log('Data:', port.read());
+      if (port.read() == "Ok pong!") {
+        document.getElementById('success').style.visibility = 'visible';
+        document.getElementById('success').textContent = "Conexão realizada com sucesso!";
+
+        document.getElementById('code').disabled = false;
+        document.getElementById('saveData').disabled = false;
+      }
+      console.log('Data Check:', port.read());
+    });
+  });
+}
+
+function saveData(port, code) {
+  var port = new serialport(port, { autoOpen: false });
+ 
+  port.open(function (err) {
+    if (err) {
+      document.getElementById('error').style.visibility = 'visible';
+      document.getElementById('error').textContent = err.message + " (" + new Date() + ")";
+
+      document.getElementById('code').value = "";
+
+      document.getElementById('code').disabled = true;
+      document.getElementById('saveData').disabled = true;
+
+      return;
+    }
+  
+    // Because there's no callback to write, write errors will be emitted on the port:
+    port.write('code:' + code);
+
+    port.on('readable', function () {
+      if (port.read() == "Ok saved!") {
+        document.getElementById('success').style.visibility = 'visible';
+        document.getElementById('success').textContent = "Código salvo com sucesso";
+
+        document.getElementById('code').disabled = false;
+        document.getElementById('saveData').disabled = false;
+      }
+      console.log('Data Saved:', port.read());
     });
   });
 
